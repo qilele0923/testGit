f4ad49ea45fafe6b729bef48286f7d6413372b51
qilele <691417830@qq.com>
2019-06-10 09:21:03 +0800

    打印master

--- src/main/java/com/test/User02.java
+++ src/main/java/com/test/User02.java
@@ -10,5 +10,7 @@ public class User02 {
         System.out.println("user05+++++");
         System.out.println("hebingwenjian");
         System.out.println("master zhuigan");
+        System.out.println("master....");
+
     }
 }
