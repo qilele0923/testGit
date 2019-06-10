6c7cf6968d9c3c598bb7edb89c9a995cae4489fb
qilele <691417830@qq.com>
2019-06-10 08:53:16 +0800

    添加一行文件

--- src/main/java/com/test/User02.java
+++ src/main/java/com/test/User02.java
@@ -6,5 +6,6 @@ package com.test;
 public class User02 {
     public static void main(String[] args) {
         System.out.println("user02___________________________");
+        System.out.println("user05+++++");
     }
 }
