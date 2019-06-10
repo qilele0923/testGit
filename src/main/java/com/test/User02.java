407310b4efc96facdb94a63d8e46aafb3454522f
qilele <691417830@qq.com>
2019-06-10 08:50:34 +0800

    新增user02

---
+++ src/main/java/com/test/User02.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by qi on  2019-06-10 8:50
+ */
+public class User02 {
+    public static void main(String[] args) {
+        System.out.println("user02");
+    }
+}
